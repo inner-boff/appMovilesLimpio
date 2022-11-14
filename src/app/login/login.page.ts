c42222a7a32e0c0a43542593db33b752702f0a6c
psoucre <pamelasoucre@gmail.com>
2022-11-14 17:09:42 -0300

    comentado login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -20,18 +20,31 @@ export class LoginPage implements OnInit {
     private router: Router,
   ) { }
 
-  // Para acceder a los campos del fromulrio:
+  /**
+   * @function email
+   * @description toma el input del usuario reactivo en el campo de email y lo guarda como credencial.
+   * @return {string} devuelve el string correspondiente al email ingresado
+   * por el usuario en el formulario reactivo.
+   */
   get email() {
-    this.authService.email = this.credentials.value.email; //nuevo
+    this.authService.email = this.credentials.value.email; 
     return this.credentials.get('email');
   }
-
+  /**
+   * @function email
+   * @description toma el input del usuario reactivo en el campo de
+   * contraseña y lo guarda como credencial.
+   * @return {string} devuelve el string correspondiente a la contraseña 
+   * ingresada por el usuario en el formulario reactivo.
+   */
   get password() {
     return this.credentials.get('password');
   }
 
-
-
+  /**
+   * @function ngOnInit
+   * @description toma las credenciales ingresadas en el formulario reactivo y aplica los validadores correspondientes.
+   */
   ngOnInit() {
     this.credentials = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -39,7 +52,13 @@ export class LoginPage implements OnInit {
     })
   }
 
-
+  /**
+   * @function login
+   * @description Se activa al presionar el botón de login. Si las
+   * credenciales ingresadas por el usuario son válidas, el usuario es
+   * redireccionado al home. Si no, el usuario permanece en la página.
+   * En cualquiera de ambos casos mostará un mensaje al usuario.
+   */
   async login() {
     const loading = await this.loadingController.create();
     await loading.present();
@@ -49,12 +68,18 @@ export class LoginPage implements OnInit {
 
     if (user) {
       this.router.navigateByUrl('/home', { replaceUrl: true });
-      console.log('usuario logueado');
+      //console.log('usuario logueado');
     } else {
       this.showAlert('Falló el login', 'Por favor intente de nuevo');
     }
   }
 
+  /**
+   * @function showAlert
+   * @param {any, any} header, message
+   * @description Muestra una alerta al usuario con un mensaje asignado
+   * según el caso. El cartel de alerta se elimina al presionar el botón OK.
+   */
   async showAlert(header, message) {
     const alert = await this.alertController.create({
       header,
